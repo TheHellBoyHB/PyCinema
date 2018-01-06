de453c6db8ece73e34be83b6c9f22db266b2c0ae
@anniethehacker <dariusilisie@gmail.com>
2018-01-06 12:10:33 +0200

    List Movies

--- src/main.py
+++ src/main.py
@@ -19,18 +19,42 @@ class Movie:
         return str(self.id) +". " + self.name
 
 
-def main():
-
+def listDeff():
     movie1 = Movie(1, "Fred", "2017")
     movie2 = Movie(2, "Chicken", "2016")
     movie3 = Movie(3, "Steve", "2015")
     movie4 = Movie(4, "Anna", "2014")
     movie5 = Movie(5, "Bell", "2013")
+    ls = [movie1, movie2, movie3, movie4, movie5]
 
-    list=[movie1,movie2,movie3,movie4,movie5]
 
-    for i in range(5):
-        print(list[i].listMovie())
+def halp():
+    print("l) List Movies/List Details")
+    print("r) Reserve Selected Movie")
+    print("h) List MyReserves")
+    print("\n")
+    print("?) Prints this menu")
+    menu()
+
+def menu():
+    cmd = input("Select Action >  ")
+    print("\n")
+
+    if cmd == "l":
+        print("Se listeaza filmele: \n")
+
+    elif cmd == "r":
+        print("Coming Soon\n")
+    elif cmd == "?":
+        halp()
+
+    menu()
+
+def main():
+    halp()
+
+
+
 
     #JSONMovies=open("movies.json","r+")
 
